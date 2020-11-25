6cbe31419f8e3324a69a553542ca1223612aefaa
Allan Luchenitser <aluchenitser@gmail.com>
2020-11-25 16:33:43 -0500

    images

--- js/magnetic_mobile.js
+++ js/magnetic_mobile.js
@@ -1,6 +1,7 @@
 magneticMobile = {
     run: function() {
         const gameElement = document.querySelector(".game .holes");
+        const holesElement = document.querySelector(".holes")
         
         gameElement.addEventListener("mousedown", e => {
             this.isMallotDown = true;
@@ -12,6 +13,13 @@ magneticMobile = {
             }, 300)
 
         })
+
+        holesElement.addEventListener("click", e => {
+            if(e.target.className.includes("image")) {
+                console.log("boink!")
+            }
+        })
+
     },
     isMallotDown: false
 }
